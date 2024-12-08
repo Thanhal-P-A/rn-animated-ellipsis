216211188649e5a321a51f1eab8e44c4515771d0
Reagan Middlebrook <reagankm@gmail.com>
2024-12-08 14:34:48 -0800

    Update configs and publish

--- jest.config.js
+++ jest.config.js
@@ -2,11 +2,10 @@ module.exports = {
     preset: 'react-native',
     setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
     transformIgnorePatterns: [
-        // Transform everything except react-native-related packages
         'node_modules/(?!(react-native|@react-native|@react-native-community|@testing-library)/)',
     ],
     transform: {
-        '^.+\\.(js|jsx|ts|tsx)$':['babel-jest', { configFile: './.babelrc' }],
+        '^.+\\.(js|jsx|ts|tsx)$': 'babel-jest',
     },
     moduleFileExtensions: ['js', 'jsx', 'ts', 'tsx'],
 };
